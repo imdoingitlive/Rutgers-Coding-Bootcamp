4a21158f6762993a29b16a1c620234bac4f25137
Edwin Rivera <edwin.rivera@outlook.com>
2016-08-03 19:42:04 -0400

    updating joinsActivity quiz

--- Week15/8-3-16-Wed/joinsActivity/questions.js
+++ Week15/8-3-16-Wed/joinsActivity/questions.js
@@ -11,15 +11,18 @@
 // 1. How would we grab every app in our database released for both Android and ios (using the string 'both', not the int 3)
 // ===
 
-
+SELECT app FROM apps LEFT JOIN os ON apps.os_id=os.id WHERE os='both';
 
 
 // 2. How would we grab only the app made by Apple (using Apple's name, not their id)
 // ===
 
-
+SELECT app FROM apps LEFT JOIN devs ON apps.d_id=devs.id WHERE dev="Apple"; 
 
 
 // 3. How would we display all app info except ids, including the name of the dev and the name of os
 // NOTE: THIS IS A HARD ONE
 // ===
+SELECT apps.app, os.os, devs.dev FROM apps 
+JOIN os ON apps.os_id=os.id 
+JOIN devs ON apps.d_id=devs.id;
